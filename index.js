8ba7e74961493f74d162d2bbbd107b067887e370
Vishoo <vverma971@gmail.com>
2025-05-13 21:46:47 +0530

    updates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 //port -> local system -> server locations 
 //localhost:8000, localhost:3000, localhost:5000
 const PORT = 3000;
-
+app.use(express.json())
 //routing 
 
 //routing -> define the endpoints
@@ -22,17 +22,111 @@ const PORT = 3000;
 //     console.log("DOM fully loaded and parsed");
 //   });
 
+const loggingMiddleware = (req, res, next) =>{
+
+    //console.log all the request data
+    const method = req.method;
+    const url = req.url;
+    const date = new Date().toLocaleString();
+    const ip = req.ip;
+    
+    console.log(`Middleware Triggered`)
+    console.log(`Request : METHOD -> ${method} URL -> ${url} DATE -> ${date} IP -> ${ip}`);
+
+    next(); 
+    //next() -> function that is used to call the next middleware in the stack
+    
+
+}
+
+
+
+
+app.use(loggingMiddleware);
+//middleware -> function that runs before the request is processed
+
 //Route 
-app.get("/", (req, res) => {
+//root route
+app.get("/",  (req, res) => {
     res.send("Hello World");
 })
 //req -> request object -> contains information about the request that was made to the server or that we have recieved from the client  
 //res -> response object -> contains information about the response that we will send back to the client
 
+//https methods
+//route structure
+//route -> url -> api -> server
+//user management api
+//user create -> data collect -> user info -> server send -> post request 
+//server recieved -> data -> server extracts and process the data -> server saves the data in the database
+
 app.get("/time", (req, res)=>{
     const date = new Date().toLocaleString();
     res.send(`The current time is ${date}`);
 });
+
+app.get("/about", (req, res)=>{
+    res.send("Hello from the about page");
+})
+
+app.post("/message", (req, res)=>{
+
+    //json file -> header, body
+    //header -> metadata -> content type, content length, content encoding, authorization 
+
+    const user_agent = req.headers['user-agent'];
+    const content_type = req.headers['content-type'];
+
+
+
+    
+
+    const message = req.body.message;
+    res.send(`${message}, ${user_agent}, ${content_type}`);
+})
+
+
+app.get("/greet/:name", (req, res)=>{
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+})
+
+
+
+//advanced routing
+
+//route parameters
+//dynamic url -> url that can change based on the request
+app.get("/user/:id", (req, res)=>{
+    //express helps us to extract the parameters from the url
+    //req.params -> object that contains the parameters from the url
+    const id = req.params.id;
+    res.send(`Hello from the user page with id ${id}`);
+})
+
+//Query Parameters
+//query parameters -> url that can change based on the request
+//query parameters -> ?key=value&key=value
+
+//query -> 
+
+
+//front end -> routing -> render UI 
+//backend -> providing specific data to the front end
+
+//query parameters -> ?key=value&key=value
+app.get("/search", (req, res)=>{
+    
+    const video = req.query.video || 'blank';//localhost:3000/search?video=496496396
+    const playlist = req.query.playlist || 'blank'; //localhost:3000/search?video=496496396&playlist=123456
+    const referrer = req.query.referrer || 'blank'; //localhost:3000/search?video=496496396&playlist=123456&referrer=google
+    res.send(`Hello from the search page with video ${video} and playlist ${playlist} and referrer ${referrer}`);
+})
+
+//user spelling error -> query parameter -> spelling error handle 
+
+
+
 
 
 
